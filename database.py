507669d8f171dd8706bb5832de09af7ae97406ee
austinwilson1296 <52784751+austinwilson1296@users.noreply.github.com>
2024-02-01 19:48:00 +0000

    added load job from db for single selection

--- database.py
+++ database.py
@@ -17,3 +17,9 @@ def load_jobs_from_db():
   for row in result.all():
       jobs.append(dict(zip(column_names, row)))
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    row = result.fetchone()
+    return row._asdict()
